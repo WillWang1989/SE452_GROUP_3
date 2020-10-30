39f37555dc27de771e7771e9d82126cedc72393a
gportil2 <35436391+gportil2@users.noreply.github.com>
2020-10-29 20:40:28 -0500

    updated inventory html and class

---
+++ src/main/java/edu/depaul/se452/windycityflyers/controller/InventoryController.java
@@ -0,0 +1,39 @@
+package edu.depaul.se452.windycityflyers.controller;
+
+import edu.depaul.se452.windycityflyers.model.Department;
+import edu.depaul.se452.windycityflyers.model.Inventory;
+import edu.depaul.se452.windycityflyers.service.DepartmentService;
+import edu.depaul.se452.windycityflyers.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/store/{storeId}/inventory")
+public class InventoryController {
+
+    @Autowired
+    private StoreService storeService;
+    @Autowired
+    private DepartmentService departmentService;
+    @GetMapping("")
+    public String showAllDepartment(@PathVariable("storeId") String storeId, Model model) {
+        model.addAttribute("store",storeService.findById(storeId));
+        return "admin/department/inventory-list";
+    }
+
+    @GetMapping("/add")
+    public String addStore(Model model) {
+        model.addAttribute("inventory", new Inventory());
+        return "admin/department/inventory-add";
+    }
+
+    @GetMapping("/edit")
+    public String editStore(Model model) {
+        model.addAttribute("inventory", new Inventory());
+        return "admin/department/inventory-edit";
+    }
+}
