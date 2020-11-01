d510420b027a29376376e1c2f069848708774524
bryan <bryanharris49@gmail.com>
2020-11-01 02:51:58 -0500

    added Customer link and payment functionality to Customer tab

--- src/main/java/edu/depaul/se452/windycityflyers/model/Payment.java
+++ src/main/java/edu/depaul/se452/windycityflyers/model/Payment.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 import java.util.List;
 
@@ -18,10 +19,15 @@ public class Payment {
     @Id
     @GeneratedValue
     private long id;
+    @NotEmpty
     private String payment_type;
     private float amount;
+    @NotEmpty
     private int card_num;
+    @NotEmpty
     private Date payment_date;
+    @NotEmpty
     private String cust_name;
+    @NotEmpty
     private long cust_id;
 }
