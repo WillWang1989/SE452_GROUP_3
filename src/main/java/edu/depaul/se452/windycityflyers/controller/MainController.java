b53218db69ed3579ed5758d0de77726fe39d1b80
BWANG35 <bwang35@depaul.edu>
2020-11-01 13:47:14 -0600

    build ui 2

--- src/main/java/edu/depaul/se452/windycityflyers/controller/MainController.java
+++ src/main/java/edu/depaul/se452/windycityflyers/controller/MainController.java
@@ -1,8 +1,11 @@
 package edu.depaul.se452.windycityflyers.controller;
 
+import edu.depaul.se452.windycityflyers.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
@@ -12,9 +15,14 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/user")
-    public String userIndex() {
-        return "user/index";
+    @GetMapping("/customer/")
+    public String customerIndex(Model model) {
+        ArrayList<Product> list = new ArrayList<>();
+        for(int i =0;i<18;i++){
+            list.add(new Product(){});
+        }
+        model.addAttribute("products",list);
+        return "customer/index";
     }
 
     @GetMapping("/search")
