5ddc88a94884e260d64d09edeb4d41f0d22f239c
BWANG35 <bwang35@depaul.edu>
2020-11-10 19:24:43 -0600

    changes on authentication and authorization

--- src/main/java/edu/depaul/se452/windycityflyers/security/SecurityConfiguration.java
+++ src/main/java/edu/depaul/se452/windycityflyers/security/SecurityConfiguration.java
@@ -1,17 +1,14 @@
 package edu.depaul.se452.windycityflyers.security;
 
-import edu.depaul.se452.windycityflyers.service.CustomerService;
+import edu.depaul.se452.windycityflyers.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -31,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
         registry.addViewController("/login").setViewName("login");
     }
     @Autowired
-    private CustomerService userService;
+    private UserServiceImpl userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/edu/depaul/se452/windycityflyers/service/CustomerServiceImpl.java
+++ src/main/java/edu/depaul/se452/windycityflyers/service/CustomerServiceImpl.java
@@ -2,30 +2,17 @@ package edu.depaul.se452.windycityflyers.service;
 
 
 import edu.depaul.se452.windycityflyers.dto.CustomerRegistrationDto;
-import edu.depaul.se452.windycityflyers.model.Admin;
 import edu.depaul.se452.windycityflyers.model.Customer;
-import edu.depaul.se452.windycityflyers.model.Runner;
-import edu.depaul.se452.windycityflyers.repository.AdminRepository;
 import edu.depaul.se452.windycityflyers.repository.CustomerRepository;
-import edu.depaul.se452.windycityflyers.repository.RunnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository userRepository;
-    @Autowired
-    private RunnerRepository runnerRepository;
-    @Autowired
-    private AdminRepository adminRepository;
-
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -42,34 +29,5 @@ public class CustomerServiceImpl implements CustomerService {
         user.setPassword(passwordEncoder.encode(registration.getPassword()));
         user.setUsername(registration.getUserName());
         return userRepository.save(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Customer user = userRepository.findByEmail(email);
-        if (user == null) {
-            user = userRepository.findByUsername(email);
-        }
-        if (user != null) {
-            return new org.springframework.security.core.userdetails.User(user.getUsername(),
-                    user.getPassword(),
-                    Arrays.asList(new SimpleGrantedAuthority("ROLE_CUSTOMER")));
-
-        }
-        Runner runner = runnerRepository.findByName(email);
-        if(runner!=null){
-            return new org.springframework.security.core.userdetails.User(runner.getName(),
-                    runner.getPassword(),
-                    Arrays.asList(new SimpleGrantedAuthority("ROLE_RUNNER")));
-        }
-        Admin admin = adminRepository.findByUsername(email);
-        if(admin!=null){
-            return new org.springframework.security.core.userdetails.User(admin.getUser_name(),
-                    admin.getPassword(),
-                    Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        }
-
-        throw new UsernameNotFoundException("Invalid username or password.");
-
     }
 }
